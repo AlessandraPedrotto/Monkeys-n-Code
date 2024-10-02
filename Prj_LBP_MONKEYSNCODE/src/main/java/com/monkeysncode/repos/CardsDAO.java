f6d0df43898c91a8b52165a469f04c7fabf145d5
alex <alexander.pelle@edu.itspiemonte.it>
2024-10-02 09:55:09 +0200

    cardsdao modified, libraries removed

--- Prj_LBP_MONKEYSNCODE/src/main/java/com/monkeysncode/repos/CardsDAO.java
+++ Prj_LBP_MONKEYSNCODE/src/main/java/com/monkeysncode/repos/CardsDAO.java
@@ -4,9 +4,7 @@ import java.util.List;
 import java.util.Optional;
 
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.data.jpa.repository.Query;
-import org.springframework.data.repository.query.Param;
-import org.springframework.web.bind.annotation.RequestParam;
+
 
 import com.monkeysncode.entites.Card;
 
